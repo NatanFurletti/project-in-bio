3b055e5d3f825402e81c9d78e8fcf5694ccd1d34
Natan Furletti <natan.furletti@outlook.com>
2025-02-13 19:39:31 -0300

    feat: add video e pricing sessions

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
 import Hero from "./components/landing-page/hero";
 import Header from "./components/landing-page/header";
+import VideoExplanation from "./components/landing-page/video-explanation";
+import Pricing from "./components/landing-page/pricing";
 
 export default function Home() {
   return (
     <div className="max-w-7x1 mx-auto border">
       <Hero />
       <Header />
-
-      {/*<VideoExplanation />
+      <VideoExplanation />
       <Pricing />
+
+      {/*
+      
       <FAQ />*/}
     </div>
   );
